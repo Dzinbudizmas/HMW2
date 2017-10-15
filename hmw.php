b6526f1e3d0a29c15c7d4ea73c494e7f0c60845a
Laima Kalinauskiene <laimucka@gmail.com>
2017-10-16 00:58:49 +0300

    autoloading 2 more

--- hmw.php
+++ hmw.php
@@ -1,15 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: delusion
- * Date: 17.10.15
- * Time: 22.51
- */
-//require 'World/Creature.php5';
 
-use World\Creatures\Creature;
+require_once __DIR__ .'/vendor/autoload.php';
 
-require __DIR__ . '/vendor/autoload.php';
+$obj = new World\Creatures\Creature();
+echo $obj.PHP_EOL;
 
-$rat = new Creature('zifara');
-echo $rat.PHP_EOL;
+$obj2 = new \World\Creatures\Sea\Fish();
+echo $obj2.PHP_EOL;
+
+$obj3 = new \World\Creatures\Land\Pet();
+echo $obj3.PHP_EOL;
+
+$obj4 = new \World\Creatures\Land\Sky\Bird();
+echo $obj4.PHP_EOL;
